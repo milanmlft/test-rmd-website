c85d80135d76f8a910dd75c5f13d2e5f8182c1fa
Milan Malfait <milan.malfait94@gmail.com>
2020-12-03 21:13:15 +0100

    Revert "Trying to reproduce error from https://github.com/statOmics/HDA2020/pull/6/checks?check_run_id=1492925915 (Part 2)"
    This reverts commit a85d1503c5958bbf6e17c7b6f542a921c67d3ccb.

--- install.R
+++ install.R
@@ -4,9 +4,8 @@ install.packages(c("remotes", "BiocManager"), Ncpus = 2L)
 BiocManager::install(
   c(
     "tidyverse", "devtools", "rmarkdown", "knitr",
-    "gganimate", "magick", "locfdr", "gridExtra"
+    "gganimate", "magick"
   ),
   Ncpus = 2L)
 
 remotes::install_github("hadley/emo")
-remotes::install_github("vqv/ggbiplot")
